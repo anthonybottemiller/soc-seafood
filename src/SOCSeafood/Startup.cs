bc8d44d52b5198be821a2678fac2ef41f1242fd9
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-02-17 10:05:41 -0800

    Add project files for test

--- src/SOCSeafood/Startup.cs
+++ src/SOCSeafood/Startup.cs
@@ -12,13 +12,10 @@ namespace SOCSeafood
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
