77b83e1f4d6f7c3a62de562e8d23d2d7d5fc7272
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-02-17 11:11:49 -0800

    Add basic configuration for MVC Entity Framework and Identity

---
+++ src/SOCSeafood/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace SOCSeafood.Models
+{
+    public class ApplicationAdministrator : IdentityUser
+    {
+    }
+}
--- src/SOCSeafood/Startup.cs
+++ src/SOCSeafood/Startup.cs
@@ -7,18 +7,45 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using SOCSeafood.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace SOCSeafood
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddEntityFramework()
+                .AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            services.AddIdentity<ApplicationAdministrator, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+            app.UseIdentity();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Account}/{action=Index}/{id?}");
+            });
 
             if (env.IsDevelopment())
             {
