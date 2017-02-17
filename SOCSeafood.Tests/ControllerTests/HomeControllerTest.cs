5258702a1fa9e8fe3105f88c24ccdf97b1a5086e
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-02-17 12:26:17 -0800

    Add test for home controller

---
+++ SOCSeafood.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SOCSeafood.Controllers;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOCSeafood.Tests.ControllerTests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            HomeController controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
