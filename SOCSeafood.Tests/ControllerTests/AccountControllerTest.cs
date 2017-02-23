3da6ec61ea8ed3f6ea733ddfe99f681d2b5b3dde
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-02-22 21:02:45 -0800

    add tests

---
+++ SOCSeafood.Tests/ControllerTests/AccountControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SOCSeafood.Controllers;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOCSeafood.Tests.ControllerTests
+{
+    public class AccountControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Login_Test()
+        {
+            AccountController controller = new AccountController();
+
+            var result = controller.Login();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Get_ViewResult_Logout_Test()
+        {
+            AccountController controller = new AccountController();
+
+            var result = controller.Logout();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+
+        [Fact]
+        public void Get_ViewResult_Register_Test()
+        {
+            AccountController controller = new AccountController();
+
+            var result = controller.Register();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+    }
+}
---
+++ SOCSeafood.Tests/ControllerTests/SubscribersControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SOCSeafood.Controllers;
+using SOCSeafood.Models;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOCSeafood.Tests.ControllerTests
+{
+    public class SubscribersControllerTest
+    {
+
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            SubscribersController controller = new SubscribersController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Get_ViewResult_Create_Test()
+        {
+            SubscribersController controller = new SubscribersController();
+
+            var result = controller.Create();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Get_ModelList_Index_Test()
+        {
+            SubscribersController controller = new SubscribersController();
+
+            IActionResult actionResult = controller.Index();
+
+            ViewResult indexView = controller.Index() as ViewResult;
+
+            var result = indexView.ViewData.Model;
+
+            Assert.IsType<List<Subscriber>>(result);
+        }
+
+    }
+}
